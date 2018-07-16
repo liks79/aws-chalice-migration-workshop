b1616864f2ac2692dfe2bd66e74ca05fbf44b632
Sungshik Jou <liks79@gmail.com>
2018-07-16 13:37:12 +0900

    Fix

--- LAB02/03.CloudAlbum-COGNITO/cloudalbum/controlloer/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-    controller.__init__.py
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    CloudAlbum is a sample application for TechSummit 2018 workshop.
-
-    :copyright: © 2018 by Sungshik Jou.
-    :license: BSD, see LICENSE for more details.
-"""
