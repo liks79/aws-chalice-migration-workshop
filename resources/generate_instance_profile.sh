faae05de70d972c80b7e4912dfcddca74f677291
Sungshik Jou <liks79@gmail.com>
2018-08-08 15:53:11 +0900

    fix

---
+++ resources/generate_instance_profile.sh
@@ -0,0 +1,8 @@
+wget https://github.com/liks79/aws-chalice-migration-workshop/raw/master/resources/workshop-cloud9-instance-profile-role-trust.json
+wget https://raw.githubusercontent.com/liks79/aws-chalice-migration-workshop/master/resources/workshop-cloud9-policy.json
+
+PARN=$(aws iam create-policy --policy-name workshop-cloud9-policy --policy-document file://workshop-cloud9-policy.json --query "Policy.Arn" --output text)
+aws iam create-role --role-name workshop-cloud9-instance-profile-role --assume-role-policy-document file://workshop-cloud9-instance-profile-role-trust.json
+aws iam attach-role-policy --role-name workshop-cloud9-instance-profile-role --policy-arn $PARN
+aws iam create-instance-profile --instance-profile-name workshop-cloud9-instance-profile
+aws iam add-role-to-instance-profile --role-name workshop-cloud9-instance-profile-role --instance-profile-name workshop-cloud9-instance-profile
